72c275a05b9ecfa107f21ffd2aeb80bf9db2d11b
Daniel Portela <danielportela.ti@gmail.com>
2024-10-21 14:44:02 -0300

    Atualizado app.component (HTML e TS)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './components/users/users.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, UsersComponent],
+  imports: [CommonModule, RouterOutlet, UsersComponent, NavbarComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

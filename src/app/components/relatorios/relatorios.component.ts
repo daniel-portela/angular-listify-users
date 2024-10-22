10ba071632cdcce8863e238a082e0bf00354f338
Daniel Portela <danielportela.ti@gmail.com>
2024-10-22 10:44:31 -0300

    Criação de pasta relatorios, layout e lógica.

--- src/app/components/relatorios/relatorios.component.ts
+++ src/app/components/relatorios/relatorios.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-relatorios',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './relatorios.component.html',
-  styleUrl: './relatorios.component.css'
+  styleUrls: ['./relatorios.component.css'],
 })
-export class RelatoriosComponent {
-
-}
+export class RelatoriosComponent {}

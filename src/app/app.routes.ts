fdfd0cec6086f0f3bef06c3a5a5659af9949280b
Daniel Portela <danielportela.ti@gmail.com>
2024-10-22 10:39:03 -0300

    Adicionar rotas de navegação dinâmica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RelatoriosComponent } from './components/relatorios/relatorios.component';
+import { UsersComponent } from './components/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'relatorios', component: RelatoriosComponent },
+  { path: 'usuarios', component: UsersComponent },
+];
